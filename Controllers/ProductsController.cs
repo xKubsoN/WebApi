da2461a9fdcf17d241d267934c85c82320492ad2
xKubsoN <kszczesny2555@gmail.com>
2025-06-04 07:20:15 +0200

    deploy.yml

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using WebApi.Models;
 [ApiController]
 [Route("api/[controller]")]
 
+
 public class ProductsController : ControllerBase {
     private readonly ApplicationDbContext _context;
 
--- obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3415c7ebc87b20847e2f51c960dd888cf9361494")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
